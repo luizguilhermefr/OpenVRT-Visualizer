ccc0d41051e6ac5b179a81b4b366af99f1598e9d
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-05-25 16:27:40 -0300

    added openlayers but not working yet

--- src/renderer/components/Visualizer.vue
+++ src/renderer/components/Visualizer.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
-    </div>
+    <div id="map"></div>
 </template>
 
 <script>
+  import ol from 'ol'
+
   export default {
     name: 'visualizer',
     computed: {
@@ -11,8 +12,35 @@
         return this.$store.state.OpenedGeoJson.data
       },
     },
+    data () {
+      return {
+        map: null,
+      }
+    },
     mounted () {
-      console.log(this.geoData)
-    }
+      this.generateOpenLayersMap()
+    },
+    methods: {
+      generateOpenLayersMap () {
+        const vectorSource = this.generateVectorSource()
+        const vectorLayer = this.generateVectorLayer(vectorSource)
+        this.map = new ol.Map({
+          target: 'map',
+          layers: [
+            vectorLayer
+          ],
+        })
+      },
+      generateVectorSource () {
+        return new ol.source.Vector({
+          features: (new ol.format.GeoJSON()).readFeatures(this.geoData())
+        })
+      },
+      generateVectorLayer(vectorSource) {
+        return new ol.layer.Vector({
+          source: vectorSource
+        })
+      }
+    },
   }
 </script>
