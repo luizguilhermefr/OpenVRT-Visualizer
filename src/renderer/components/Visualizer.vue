60e7f3559681525f5c38c354bdfa32eb54b73e1b
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-05-30 14:17:56 -0300

    osm

--- src/renderer/components/Visualizer.vue
+++ src/renderer/components/Visualizer.vue
@@ -1,13 +1,31 @@
 <template>
-    <div id="map"></div>
+    <div id="map" class="md-elevation-2"></div>
 </template>
+
+<style scoped>
+    #map {
+        width: 95%;
+        height: 900px;
+        margin: 2%;
+        background-color: cornsilk;
+    }
+</style>
 
 <script>
   import Map from 'ol/map'
-  import VectorSource from 'ol/source/vector'
   import GeoJSON from 'ol/format/geojson'
-  import Vector from 'ol/layer/vector'
   import View from 'ol/view'
+
+  import Vector from 'ol/layer/vector'
+  import Tile from 'ol/layer/tile'
+
+  import Proj from 'ol/proj'
+  import Control from 'ol/control'
+
+  import OSM from 'ol/source/osm'
+  import VectorSource from 'ol/source/vector'
+
+  import Styles from './styles/mapstyle'
 
   export default {
     name: 'visualizer',
@@ -29,19 +47,31 @@
         this.map = new Map({
           target: 'map',
           layers: [
+            new Tile({
+              source: new OSM()
+            }),
             this.generateVectorLayer(),
           ],
+          controls: Control.defaults({
+            attributionOptions: {
+              collapsible: false,
+            },
+          }),
           view: new View({
-            center: [
-              -8.810269960287718,
-              115.23514275881486,
-            ],
+            center: Proj.fromLonLat([
+              121.767277, -3.963446,
+            ]),
+            zoom: 2,
           }),
         })
+      },
+      style (feature) {
+        return Styles[feature.getGeometry().getType()]
       },
       generateVectorLayer () {
         return new Vector({
           source: this.generateVectorSource(),
+          style: this.style
         })
       },
       generateVectorSource () {
---
+++ src/renderer/components/styles/mapstyle.js
@@ -0,0 +1,46 @@
+import Style from 'ol/style/style'
+import Stroke from 'ol/style/stroke'
+import Fill from 'ol/style/fill'
+
+export default {
+  'LineString': new Style({
+    stroke: new Stroke({
+      color: 'green',
+      width: 1,
+    }),
+  }),
+  'MultiLineString': new Style({
+    stroke: new Stroke({
+      color: 'green',
+      width: 1,
+    }),
+  }),
+  'MultiPolygon': new Style({
+    stroke: new Stroke({
+      color: 'yellow',
+      width: 1,
+    }),
+    fill: new Fill({
+      color: 'rgba(255, 255, 0, 0.1)',
+    }),
+  }),
+  'Polygon': new Style({
+    stroke: new Stroke({
+      color: 'blue',
+      lineDash: [4],
+      width: 3,
+    }),
+    fill: new Fill({
+      color: 'rgba(0, 0, 255, 0.1)',
+    }),
+  }),
+  'GeometryCollection': new Style({
+    stroke: new Stroke({
+      color: 'magenta',
+      width: 2,
+    }),
+    fill: new Fill({
+      color: 'magenta',
+    }),
+  }),
+}
